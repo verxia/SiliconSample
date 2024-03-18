ffbfcdc39211a94d298e8d87fa5ba25fa774785c
verxia <76971655+verxia@users.noreply.github.com>
2024-03-18 17:15:52 -0400

    Update file_paths.py

--- file_paths.py
+++ file_paths.py
@@ -5,5 +5,3 @@ PERSONAS_PATH = 'ANES_argyle/results/personas.json'
 SS_RESPONSES_PATH = 'ANES_argyle/results/ss_responses.csv'
 EVALUATIONS_PATH = 'ANES_argyle/results/evaluations.csv'
 SURVEY_KEYS = ["vote"] # what questions do we want to ask ChatGPT?
-
-API_KEY = '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'
